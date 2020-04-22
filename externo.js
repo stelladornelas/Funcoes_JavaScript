c2d13c1759190b3ebe18c2aeb37f743f50f3777c
stelladornelas <stella.lamounier@gmail.com>
2020-04-22 15:45:04 -0300

    Create externo.js

---
+++ externo.js
@@ -0,0 +1,37 @@
+
+  // Citar
+  // function teste(){
+  
+  // document.write("ok");
+  
+  // }
+
+  // function teste2(){
+     
+
+  //  alert("bem vindo");
+    
+  //   }
+
+
+  //   function teste3(){
+     
+  //     document.write ("mostrando terceira opção de chamar função extenra JS")
+       
+  //      }
+   
+       
+    function somavalor(n1, n2){
+     
+      var soma;
+      n1 = parseFloat( n1);
+      n2 = parseFloat(n2);
+      soma = parseFloat(n1 + n2)       
+       alert("Soma " + soma + "");
+       }
+
+       var n1 = prompt ("digite o valor 01: ")
+       var n2 = prompt ("digite o valor 02: ")
+
+       somavalor( n1, n2);
+   
